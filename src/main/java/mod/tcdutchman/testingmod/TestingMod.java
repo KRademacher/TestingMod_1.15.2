5f59e509f47bb7ab450198d20b3f4acc22e704a5
KRademacher <Kevin_Rademacher@hotmail.com>
2020-09-23 13:47:52 +0200

    initial commit

---
+++ src/main/java/mod/tcdutchman/testingmod/TestingMod.java
@@ -0,0 +1,11 @@
+package mod.tcdutchman.testingmod;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(TestingMod.MOD_ID)
+public class TestingMod {
+	
+	public static final String MOD_ID = "testingmod";
+	
+	
+}
